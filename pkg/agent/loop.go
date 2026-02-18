af78970cbe0127e57432cf7729cbf24411f8de14
afjcjsbx <afjcjsbx@gmail.com>
2026-02-19 00:06:52 +0100

    add stock tool

--- pkg/agent/loop.go
+++ pkg/agent/loop.go
@@ -116,6 +116,9 @@ func registerSharedTools(
 		agent.Tools.Register(tools.NewI2CTool())
 		agent.Tools.Register(tools.NewSPITool())
 
+		// Stock tool
+		agent.Tools.Register(tools.YFinanceStockTool())
+
 		// Message tool
 		messageTool := tools.NewMessageTool()
 		messageTool.SetSendCallback(func(channel, chatID, content string) error {
