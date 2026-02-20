5247c2fdade90f8072f84c7d23111d904fe88d45
afjcjsbx <afjcjsbx@gmail.com>
2026-02-20 20:39:12 +0100

    fixed duplicated tool

--- pkg/agent/loop.go
+++ pkg/agent/loop.go
@@ -109,9 +109,6 @@ func registerSharedTools(cfg *config.Config, msgBus *bus.MessageBus, registry *A
 		// Stock tool
 		agent.Tools.Register(tools.YFinanceStockTool())
 
-		// Stock tool
-		agent.Tools.Register(tools.YFinanceStockTool())
-
 		// Message tool
 		messageTool := tools.NewMessageTool()
 		messageTool.SetSendCallback(func(channel, chatID, content string) error {
