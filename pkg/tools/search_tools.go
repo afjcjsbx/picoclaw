b64d88f63a48dd94e041c9c64ad73d0f1bd727a3
afjcjsbx <afjcjsbx@gmail.com>
2026-02-28 22:50:12 +0100

    fix tool description

--- pkg/tools/search_tools.go
+++ pkg/tools/search_tools.go
@@ -77,7 +77,7 @@ func (t *BM25SearchTool) Name() string {
 
 func (t *BM25SearchTool) Description() string {
 	// return "Search available tools on-demand using natural language query. Returns JSON schemas of discovered tools."
-	return "CRITICAL TOOL: You have a massive catalog of hidden tools. If you lack a specific tool to fulfill the user's request, you MUST use this tool to search the catalog using natural language (e.g. query='github repos', 'weather forecast', 'database'). It unlocks the tools so you can use them normally."
+	return "Searches the tool library for useful tools. Use this when you don't have a tool for a specific task. Returns a list of tools that match the description"
 }
 
 func (t *BM25SearchTool) Parameters() map[string]any {
